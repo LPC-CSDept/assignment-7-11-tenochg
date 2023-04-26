7bc9347682fc262d961b4070ad0051c7fa5b68c7
tenochg <yomoozuki@gmail.com>
2023-04-26 11:09:59 -0700

    711

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 Math = [100, 90]
 English = [90, 80]
@@ -14,5 +15,18 @@ names = ['Bill', 'Mary']
 # Make your code
 # ******************************
 
+fig, ax = plt.subplots()
+bar1 = ax.bar(x-width*1.5, Math, width)
+bar2 = ax.bar(x-width*0.5, English, width)
+bar3 = ax.bar(x+width*1.5, Physics, width)
+bar4 = ax.bar(x+width*0.5, Computer, width)
+
+ax.set_title("Grouped graph for scores")
+ax.bar_label(bar1, padding=3)
+ax.bar_label(bar2, padding=3)
+ax.bar_label(bar3, padding=3)
+ax.bar_label(bar4, padding=3)
+ax.legend(labels)
+ax.set_xticks(x, names)
 
 fig.savefig('A11.png')
